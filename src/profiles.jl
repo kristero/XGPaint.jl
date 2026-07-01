cbf5cbdef817cdbc1204712afd76966177c73d5c
Kristers Nagainis <kristers.nagainis@lu.lv>
2026-07-01 23:11:45 +0900

    Add cluster version of XGPaint

--- src/profiles.jl
+++ src/profiles.jl
@@ -36,8 +36,8 @@ end
 Base.show(io::IO, w::AbstractProfileWorkspace) = print(io, "$(typeof(w))")
 
 
-function profile_grid(model::AbstractGNFW{T}; N_z=256, N_logM=256, N_logθ=512, z_min=1e-3, 
-        z_max=5.0, logM_min=11, logM_max=15.7, logθ_min=-15.7, logθ_max=2.5) where T
+function profile_grid(model::AbstractGNFW{T}; N_z=256, N_logM=128, N_logθ=256, z_min=1e-3, 
+        z_max=5.0, logM_min=12, logM_max=15.7, logθ_min=-15.7, logθ_max=2.5) where T
 
     logθs = LinRange(logθ_min, logθ_max, N_logθ)
     redshifts = LinRange(z_min, z_max, N_z)
@@ -226,7 +226,7 @@ Base.show(io::IO, ip::LogInterpolatorProfile{T,P,I1}) where {T,P,I1} = print(
 
 """Helper function to build a (θ, z, Mh) interpolator"""
 function build_interpolator(model::AbstractProfile; cache_file::String="", 
-                            N_logθ=512, pad=256, logM_max = 15.7, overwrite=true, verbose=true)
+                            N_logθ=256, pad=128, logM_max = 15.7, overwrite=true, verbose=true)
 
     if overwrite || (isfile(cache_file) == false)
         verbose && print("Building new interpolator from model.\n")
